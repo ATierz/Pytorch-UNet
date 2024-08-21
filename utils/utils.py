8ad065e27e0f37e849366cef6600bfb51a633ff5
atierz <atl_alicia96@hotmail.com>
2024-08-21 09:47:25 +0200

    First commit

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,9 @@
 import matplotlib.pyplot as plt
+import wandb
+import torch
+from torchvision.utils import make_grid
+
+
 
 
 def plot_img_and_mask(img, mask):
@@ -11,3 +16,20 @@ def plot_img_and_mask(img, mask):
         ax[i + 1].imshow(mask == i)
     plt.xticks([]), plt.yticks([])
     plt.show()
+
+
+def log_inference_example(model, dataloader, device):
+    model.eval()
+    for batch in dataloader:
+        inputs = batch['image'].to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
+        with torch.no_grad():
+            outputs = model(inputs)
+        # Selecciona la primera imagen del batch para inferencia
+        input_image = inputs[0].cpu()
+        output_image = outputs[0].cpu()
+        cosi = (output_image.argmax(dim=0)).long().squeeze()
+
+        # grid = make_grid([input_image[0, :, :], cosi], nrow=2)
+        grid = make_grid([input_image[0, :, :].unsqueeze(0), cosi.unsqueeze(0)], nrow=2).numpy() * 255
+
+        return grid.transpose(1, 2, 0)
